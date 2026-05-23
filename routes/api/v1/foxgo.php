2534a5669e565279d9a559b6ecb93f6a2082562d
comercialpeakburguer-tech <comercialpeakburguer@gmail.com>
2026-05-22 21:57:10 -0300

    feat(admin): expose v3.9 earning report api routes

--- routes/api/v1/foxgo.php
+++ routes/api/v1/foxgo.php
@@ -3,10 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api\\V1', 'middleware' => 'localization'], function () {
+    Route::get('app-download-section', 'ConfigController@app_settings_download_section');
+
     Route::group(['prefix' => 'delivery-man', 'middleware' => 'actch:deliveryman_app'], function () {
         Route::group(['middleware' => ['dm.api']], function () {
             Route::get('available-requests', 'DeliverymanController@available_requests');
             Route::put('release-to-another-deliveryman', 'DeliverymanController@foxgo_release_to_another_deliveryman');
+            Route::get('new-earning-report', 'DeliverymanEarningReportController@getEarningReport');
         });
+    });
+
+    Route::group(['prefix' => 'vendor', 'namespace' => 'Vendor', 'middleware' => ['vendor.api', 'actch:vendor_app']], function () {
+        Route::get('earning-report', 'StoreEarningReportController@getEarningReport');
     });
 });
