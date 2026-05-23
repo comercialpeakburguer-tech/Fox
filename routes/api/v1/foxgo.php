7095c47320c5890d79c30ddf00b4b027d2db5923
comercialpeakburguer-tech <comercialpeakburguer@gmail.com>
2026-05-22 22:23:28 -0300

    feat(reels): expose foxgo reels api routes

--- routes/api/v1/foxgo.php
+++ routes/api/v1/foxgo.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Api\\V1', 'middleware' => 'localization'], function () {
     Route::get('app-download-section', 'FoxGoConfigController@appDownloadSection');
 
+    Route::group(['prefix' => 'reels'], function () {
+        Route::get('/', 'FoxGoReelController@index');
+    });
+
     Route::group(['prefix' => 'delivery-man', 'middleware' => 'actch:deliveryman_app'], function () {
         Route::group(['middleware' => ['dm.api']], function () {
             Route::get('available-requests', 'DeliverymanController@available_requests');
@@ -15,6 +19,12 @@ Route::group(['namespace' => 'Api\\V1', 'middleware' => 'localization'], functio
 
     Route::group(['prefix' => 'vendor', 'namespace' => 'Vendor', 'middleware' => ['vendor.api', 'actch:vendor_app']], function () {
         Route::get('earning-report', 'StoreEarningReportController@getEarningReport');
+    });
+
+    Route::group(['prefix' => 'vendor', 'middleware' => ['vendor.api', 'actch:vendor_app']], function () {
+        Route::get('reels', 'FoxGoReelController@vendorList');
+        Route::post('reels/store', 'FoxGoReelController@vendorStore');
+        Route::put('reels/status', 'FoxGoReelController@vendorStatus');
     });
 
     Route::group(['prefix' => 'customer', 'middleware' => ['auth:api']], function () {
