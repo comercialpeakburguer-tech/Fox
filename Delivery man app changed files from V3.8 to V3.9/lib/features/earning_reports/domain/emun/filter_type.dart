44f50d5379b7f6d2e3dc9ee1b5305dce0ad87984
comercialpeakburguer-tech <comercialpeakburguer@gmail.com>
2026-05-24 12:04:45 -0300

    Delete Delivery man app changed files from V3.8 to V3.9 directory

--- Delivery man app changed files from V3.8 to V3.9/lib/features/earning_reports/domain/emun/filter_type.dart
+++
@@ -1,8 +0,0 @@
-enum FilterType {
-  all,
-  thisYear,
-  previousYear,
-  thisMonth,
-  thisWeek,
-  custom,
-}
--- Delivery man app changed files from V3.8 to V3.9/lib/features/earning_reports/domain/services/report_service_interface.dart
+++
@@ -1,5 +0,0 @@
-import 'package:sixam_mart_delivery/features/earning_reports/domain/models/earning_report_model.dart';
-
-abstract class EarningReportServiceInterface {
-  Future<EarningReportModel?> getEarningReport({required int offset, required String? from, required String? to, required bool isDelivery});
-}
